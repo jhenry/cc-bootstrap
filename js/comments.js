a9c1065e1bd3fa0358d1defa77f0e8b4cc5e5165
Justin Henry <justin.henry@gmail.com>
2019-11-08 19:11:47 -0500

    Basic comment posting and display.

--- js/comments.js
+++ js/comments.js
@@ -4,40 +4,25 @@ class Comment {
 		this.replyText = $('meta[name="reply"]').attr('content');
 		this.reportAbuseText = $('meta[name="report_abuse"]').attr('content');
 
+		this.templateUrl = cc.themeUrl + '/blocks/comment.html';
+
 		this.lastCommentId = $('#comments-list-block > div:last-child').data('comment');
 		this.commentCount = Number($('#comments .totals span').text());
 		this.loadMoreComments = (this.commentCount > 5) ? true : false;
-		this.setUpCardTemplate(cc.themeUrl + '/blocks/comment.html');
-		this.cardTemplate = $('#comments-list-block .template'); 
+		this.cardTemplate = $("#comments-list-block .template"); 
 	}
 	// Retrieve an HTML template for the comment card structure.
-	setUpCardTemplate(templateUrl) {
+	setUpCardTemplate() {
 		let callback = response => $('#comments-list-block').append(response); 
-		$.get(templateUrl).done(callback);
+		$.get(this.templateUrl).done(callback);
 	}
-
-}
-
-var comment = new Comment();
-
-//console.log(comment.templateHTML);
-
-var comments = {
-        lastCommentId : $('.commentList > div:last-child').data('comment'),
-        commentCount : Number($('#comments .totals span').text()),
-        loadMoreComments : (this.commentCount > 5) ? true : false,
-
 	
-
-	
-
-	// Show a new comment after it is posted.
-	showNew: function(responseData, commentForm) {
+	appendNew(responseData, commentForm) {
 		this.resetCommentForms(commentForm);
 		// Append new comment if auto-approve comments is on
 		if (responseData.other.autoApprove === true) {
-			var cardElement = this.buildCard(this.cardTemplate, responseData.other.commentCard);
-			var card = responseData.other.commentCard;
+			let cardData = responseData.other.commentCard;
+			let cardElement = this.buildCard(cardData);
 
 			// Remove 'no comments' message if present 
 			$('#no-comments').remove();
@@ -45,26 +30,31 @@ var comments = {
 			// Update comment count text
 			$('#comment-count').text(responseData.other.commentCount);
 
+			// Un-hide and remove .template class
+			cardElement.toggleClass('d-none');
+			cardElement.toggleClass('template');
+
+
 			// Append comment to list
-			if (card.comment.parentId !== 0) {
-				var parentComment = $('[data-comment="' + card.comment.parentId + '"]');
-				this.indent(card, cardElement, parentComment);
+			if (cardData.comment.parentId !== 0) {
+				let parentComment = $('[data-comment="' + cardData.comment.parentId + '"]');
+				this.indent(cardData, cardElement, parentComment);
 				parentComment.after(cardElement);
 			} else {
-				$('#comments-list-block').append(cardElement);
+				$("#comments-list-block").append(cardElement);
 			}
 		}
-	},
+	}
 
 	/**
-	 * Generates comment card dom structure and content to be appended to comment list on play page
+	 * Generates comment card dom structure and content 
 	 * @param object cardData The CommentCard object for the comment being appended
 	 * @return object the jQuery object for the newly filled comment card element
 	 */	
-	buildCard: function(cardData) {
-		var card = this.cardTemplate;
+	buildCard(cardData) {
+		let card = this.cardTemplate;
 		card.attr('data-comment', cardData.comment.commentId);
-	
+
 		this.buildCardAvatar(card, cardData);
 		this.buildCardAuthorLink(card, cardData);
 		this.buildCardDate(card, cardData);
@@ -73,40 +63,41 @@ var comments = {
 		this.buildCardBodyText(card, cardData);
 
 		return card;
-	},
+	}
+
 	// Add text to comment card
-	buildCardBodyText: function(card, cardData) {
+	buildCardBodyText(card, cardData) {
 		card.find('.comment-text').text(cardData.comment.comments);
-	},
+	}
 	// Fill in action links
-	buildCardActions: function(card, cardData) {
+	buildCardActions(card, cardData) {
 		card.find('.commentAction .reply').text(this.replyText);
 		card.find('.flag').text(this.reportAbuseText).attr('data-id', cardData.comment.commentId);
-	},
+	}
 	// Set the comment date text
-	buildCardDate: function(card, cardData) {
+	buildCardDate(card, cardData) {
 		let commentDate = new Date(cardData.comment.dateCreated.split(' ')[0]);
-		monthPadding = (String(commentDate.getMonth()+1).length === 1) ? '0' : '';
-		datePadding = (String(commentDate.getDate()).length === 1) ? '0' : '';
+		let monthPadding = (String(commentDate.getMonth()+1).length === 1) ? '0' : '';
+		let datePadding = (String(commentDate.getDate()).length === 1) ? '0' : '';
 		card.find('.commentDate').text(monthPadding + (commentDate.getMonth()+1) + '/' + datePadding + commentDate.getDate() + '/' + commentDate.getFullYear());
-	},
+	}
 
 	// Set link for the comment author's member page.
-	buildCardAuthorLink: function(card, cardData) {
+	buildCardAuthorLink(card, cardData) {
 		let memberUrl = cc.baseUrl + '/members/' + cardData.author.username;
 		card.find('.authorUrl').attr('href', memberUrl).text(cardData.author.username);
-	},
+	}
 
 	// Set an avatar if the user has uploaded one.
-	buildCardAvatar: function(card, cardData) {
+	buildCardAvatar(card, cardData) {
 		if (cardData.avatar !== null) {
 			card.find('.card-img').attr('src', cardData.avatar).toggle('.d-none');
 			card.find('.default-avatar').toggle('.d-none');
 		}
-	},
+	}
 	
 	// Fill in the Reply To: User text
-	buildCardReplyTo: function(card, cardData) {
+	buildCardReplyTo(card, cardData) {
 		if (cardData.comment.parentId !== 0) {
 			card.find('.commentReply').text(cumulusClips.replyToText + ' ');
 			let anchor = {
@@ -118,10 +109,10 @@ var comments = {
 		} else {
 			card.find('.commentReply').remove();
 		}
-	},
+	}
 	// Apply indent class
-	indent: function(card, cardElement, parentComment) {
-		var indentClass;
+	indent(card, cardElement, parentComment) {
+		let indentClass = null;
 		if (parentComment.hasClass('commentIndentTriple') || parentComment.hasClass('commentIndentDouble')) {
 			indentClass = 'commentIndentTriple';
 		} else if (parentComment.hasClass('commentIndent')) {
@@ -130,22 +121,28 @@ var comments = {
 			indentClass = 'commentIndent';
 		}
 		cardElement.addClass(indentClass);
-	},
+	}
 
 	// stub
-	resetCommentForms: function () {
+	resetCommentForms() {
 		// if it's a reply, remove the reply form
 
 		// clear out top-level form
 	}
-}; 
+
+}
+
+
+let comment = new Comment();
+comment.setUpCardTemplate();
 
 // Submit 'comment form' and attach new comment to thread
-$('#addComment').submit(function(event){
+$('#comments').on("submit", "form", function(event){
 	var url = cc.baseUrl + '/actions/comment/add/';
 	var commentForm = $(this);
 	var callback = function(responseData) {
-		comments.showNew(responseData, commentForm);
+		let comment = new Comment();
+		comment.appendNew(responseData, commentForm);
 	}
 
 	cc.executeAjax(url, $(this).serialize(), callback);
