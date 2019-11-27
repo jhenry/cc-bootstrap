e91316e6f9f466942959d6ddb1c3e5f02e04c843
Justin Henry <justin.henry@gmail.com>
2019-11-27 12:53:43 -0500

    Moved related videos to it's own block.

---
+++ blocks/watch/related.php
@@ -0,0 +1,32 @@
+            <div class="related-content mt-3">
+                <h3><?= Language::getText('suggestions_header') ?></h3>
+                <?php if (count($relatedVideos) > 0) : ?>
+
+                    <div class="related-video-list">
+                        <?php $videoService = $this->getService('Video'); ?>
+                        <ul class="list-unstyled">
+
+                            <?php foreach ($relatedVideos as $relatedVideo) : ?>
+                                <?php $relatedVideoUrl = $videoService->getUrl($relatedVideo);  ?>
+                                <li class="media my-2">
+                                    <div class="related-thumb-container d-flex bg-dark justify-content-center mr-2">
+                                        <a href="<?= $relatedVideoUrl  ?>" class="related-thumb-link">
+                                            <img class="related-mini-thumb" style="max-width: 128px; height: 86px;" src="<?= getVideoThumbUrl($relatedVideo); ?>" alt="">
+                                        </a>
+                                        <?php sscanf($relatedVideo->duration, "%d:%d:%d", $hours, $minutes, $seconds); ?>
+                                        <small class="duration bg-dark text-light px-1" aria-label="<?= durationInWords($relatedVideo->duration); ?>"><?= $relatedVideo->duration ?></small>
+                                    </div>
+                                    <div class="media-body">
+                                        <p class="mt-0 mb-1">
+                                            <a href="<?= $relatedVideoUrl ?>"><?php echo htmlspecialchars($relatedVideo->title); ?></a>
+                                        </p>
+                                    </div>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+
+                <?php else : ?>
+                    <strong><?= Language::getText('no_suggestions') ?></strong>
+                <?php endif; ?>
+            </div>
