72cbf13387418703a8afa82e1eafbaa3d8bedcb1
Justin Henry <justin.henry@gmail.com>
2019-12-04 15:13:30 -0500

    Card templating. Added Momentjs for duration.

--- js/profile.js
+++ js/profile.js
@@ -4,19 +4,12 @@ class Profile {
     this.watchAllText = $('meta[name="watch_all"]').attr('content')
     this.watchLaterText = $('meta[name="watch_later"]').attr('content')
 
-    this.thumbUrl = $('meta[name="thumbUrl"]').attr('content');
-    this.videoCount = Number($('meta[name="videoCount"]').attr('content'));
-    this.playlistCount = Number($('meta[name="playlistCount"]').attr('content'));
-    this.watchLaterPlaylistId = $('meta[name="watchLaterPlaylistId"]').attr('content');
+    this.thumbUrl = $('meta[name="thumbUrl"]').attr('content')
+    this.videoCount = Number($('meta[name="videoCount"]').attr('content'))
+    this.playlistCount = Number($('meta[name="playlistCount"]').attr('content'))
+    this.watchLaterPlaylistId = $('meta[name="watchLaterPlaylistId"]').attr('content')
 
-    this.videoTemplateUrl = cc.themeUrl + '/templates/video_card.html'
     this.videoRequestUrl = cc.baseUrl + '/members/videos'
-
-    //this.userId = this.getLastCommentId()
-    // this.retrieveOffset = $('#member-videos .video').length;
-    // this.commentCount = Number($('#comment-count').text())
-    // this.loadMoreComments = (this.commentCount > 5) ? true : false
-  //  this.cardTemplate = $("#video-card-template")
   }
 
   chunker(arr, chunkSize) {
@@ -26,7 +19,7 @@ class Profile {
       sets[i] = arr.slice(j, j + chunkSize)
     }
     
-    return sets;
+    return sets
   }
 
   loadVideoGrid(videoList) {
@@ -35,52 +28,89 @@ class Profile {
     const videoRows = this.chunker(videoList, columns)
     for (const videos of videoRows) {
       const row = $('<div class="row pt-4"></div>')
-      for (const video of videos) {
+      for (let video of videos) {
+        video = this.loadVideoMeta(video);
         const col = $('<div class="col-md-' + columnWidth + ' video"></div>')
-        const template = $.templates('#video-card-template');
-        const renderedCard = template.render(video);
+        const template = $.templates('#video-card-template')
+        const renderedCard = template.render(video)
+
         col.append(renderedCard)
         row.append(col)
-        $('#videos_list').append(row);
+        $('#videos_list').append(row)
       }
 
     }
   }
 
+  loadVideoMeta (video) {
+    video.thumbUrl = this.thumbUrl
+    video.url = this.getVideoUrl(video)
+    video.truncatedDescription = this.truncateDescription(video.description)
+    video.durationInWords = moment.duration(this.formatDuration(video.duration)).humanize()
+    video.memberBaseUrl = cc.baseUrl + '/members/'
+
+    return video
+  }
+
+  truncateDescription (description) {
+    if (description.length > 250) {
+      description = description.substring(0, 250) + '...'
+    }
+    return description
+  }
+
+  formatDuration (duration) {
+    let fullDuration = duration
+    if (duration.match(/:/g).length === 1) {
+      fullDuration = "0:" + duration 
+    }
+    return fullDuration
+  }
+
+  /**
+ * Retrieve the full URL to a video
+ * @param object video The video whose URL is being retrieved
+ * @return string Returns the complete URL to given video
+ */
+  getVideoUrl(video) {
+    let url = cc.baseUrl
+    url += '/watch/' + video.videoId + '/'
+    url += this.generateSlug(video.title) + '/'
+    return url
+  }
+
+  /**
+   * Generate a URL friendly slug from an input string
+   * @param string stringToConvert The string to convert into a URL slug
+   * @return string Returns a string with non alphanum characters converted to hyphens.
+   */
+  generateSlug(stringToConvert) {
+    let slug = stringToConvert.replace(/[^a-z0-9]+/ig, '-')
+    slug = slug.replace(/^-|-$/g, '').toLowerCase()
+    return slug
+  }
 }
 
 const profile = new Profile()
-//profile.setUpCardTemplate(profile.videoTemplateUrl)
-
-
 
 $('#member-videos').on('click', '.loadMoreVideos button', function (event) {
-  var loadMoreButton = $(this);
-  var userId = loadMoreButton.data('user');
-  var retrieveOffset = $('#videos_list .video').length;
-  var retrieveLimit = Number(loadMoreButton.data('limit'));
+  const loadMoreButton = $(this)
+  const userId = loadMoreButton.data('user')
+  const retrieveOffset = $('#videos_list .video').length
+  const retrieveLimit = Number(loadMoreButton.data('limit'))
   $.ajax({
-    url: cc.baseUrl + '/members/videos',
+    url: profile.videoRequestUrl,
     data: { userId: userId, start: retrieveOffset, limit: retrieveLimit },
     dataType: 'json',
     success: function (responseData, textStatus, jqXHR) {
       // Append video cards
-profile.loadVideoGrid(responseData.other.videoList)
-      // $.each(responseData.other.videoList, function (index, value) {
-      //   var template = $.templates('#video-card-template');
+      profile.loadVideoGrid(responseData.other.videoList)
 
-      //   var htmlOutput = template.render(value);
-
-      //   $('#videos_list').append(htmlOutput);
-      //   //var videoCard = buildVideoCard(cumulusClips.videoCardTemplate, value);
-      //   //$('.videos_list').append(videoCard);
-      // });
-
-      // Remove load more button
+      // Remove load more button if we're out of videos to load
       if ($('#videos_list .video').length === profile.videoCount) {
-        loadMoreButton.remove();
+        loadMoreButton.remove()
       }
     }
-  });
-  event.preventDefault();
+  })
+  event.preventDefault()
 })
